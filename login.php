694cf51d9d18f8905b4c857738810b35a423b968
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-22 22:30:13 +0000

    style: login page

--- login.php
+++ login.php
@@ -17,26 +17,28 @@
 	<title>Book Keeper Login</title>
 </head>
 <body>
-	<div class="login-form-container">
-		<div class="login-header"><h3>Login</h3></div>
-		<form  method="POST" action="includes/login.inc.php">
-			<?php
-				if(isset($_GET["error"])) {
-					if($_GET["error"] == "emptyfields") {
-						echo "<p class=\"error-msg\">Preenhce todos os campos pá!</p>";
-					} else if($_GET["error"] == "wrongpwd") {
-						echo "<p class=\"error-msg\">Você falhou, tente novamente!</p>";
-					} else if($_GET["error"] == "loggedout") {
-						echo "<p class=\"error-msg\">O senhor foi logged out da sua maravilhosa dashboard!</p>";
-					} /*else if($_GET["error"] == "nomatch") {
-						echo "<p class=\"error-msg\">As passwords não estão batendo certo!</p>";
-					}*/
-				}
-			?>
-			<input type="text" name="id" placeholder="Nome de Utilizador">
-			<input type="password" name="pwd" placeholder="Password">
-			<button  class="login-submit" type="submit" name="login-submit">Login</button>
-		</form>    		
+	<div class="login-body">
+		<div class="login-form-container">
+			<!-- <div class="login-header"><h3>Login</h3></div> -->
+			<img src="./imgs/logo.png">
+			<h2 class="login-title">Book Keeper</h2>
+			<form  method="POST" action="includes/login.inc.php">
+				<?php
+					if(isset($_GET["error"])) {
+						if($_GET["error"] == "emptyfields") {
+							echo "<p class=\"error-msg\">Preenhce todos os campos pá!</p>";
+						} else if($_GET["error"] == "wrongpwd") {
+							echo "<p class=\"error-msg\">Você falhou, tente novamente!</p>";
+						} else if($_GET["error"] == "loggedout") {
+							echo "<p class=\"error-msg\">O senhor foi logged out da sua maravilhosa dashboard!</p>";
+						}
+					}
+				?>
+				<input type="text" name="id" placeholder="Nome de Utilizador">
+				<input type="password" name="pwd" placeholder="Password">
+				<button  class="login-btn" type="submit" name="login-submit">Sign in</button>
+			</form>    		
+		</div>
 	</div>
 </body>
 </html>
