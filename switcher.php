36898d196eba316af5b61d04612952a7f5de980d
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-27 00:51:04 +0000

    create switcher tab

--- switcher.php
+++ switcher.php
@@ -59,7 +59,45 @@
 			align-items: center;
 			justify-content: space-around;
 			height: 3em;
+			margin-top: 2rem;
+			padding: 0.5rem;
+
+			box-shadow: rgba(2, 8, 20, 0.1) 0px 0.175rem 0.5em;
+			transform: translateX(-40%);
+			transition: opacity 0.15s ease-out;
 		}
+
+		.has-dropdown:focus-within .dropdown {
+			opacity: 1;
+			pointer-events: auto;
+		}
+
+		.dropdown-item a {
+			width: 100%;
+			height: 100%;
+			size: 0.7rem;
+			padding-left: 10px;
+			font-weight: bold;
+		}
+		.dropdown-item a::before {
+			content: ' ';
+			border: 2px solid white;
+			border-radius: 50%;
+			width: 2rem;
+			height: 2rem;
+			display: inline-block;
+			vertical-align: middle;
+			margin-right: 10px;
+		}
+
+		:root {
+
+		}
+
+		.light {
+			
+		}
+
 	</style>
 </head>
 <body class="light">
