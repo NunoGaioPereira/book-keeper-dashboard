ae3e37acbc6c2c3350ff06b999c9f5555fdc5e43
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-31 23:55:38 +0100

    add js to switch theme and keep it in storage

--- switcher.php
+++ switcher.php
@@ -182,6 +182,51 @@
 
 
 	<script defer type="text/javascript">
-</script>
+		const darkButton = document.getElementById('dark');
+		const lightButton = document.getElementById('light');
+		const solarButton = document.getElementById('solar');
+		const body = document.body;
+
+		// Apply cached theme on reload
+		const theme = localStorage.getItem('theme');
+		const isSolar = localStorage.getItem('isSolar');
+
+		if (theme) {
+		  body.classList.add(theme);
+		  isSolar && body.classList.add('solar');
+		}
+
+
+		darkButton.onclick = () => {
+			// body.classList.remove('light');
+			// body.classList.add('dark');
+			body.classList.replace('light', 'dark');
+			localStorage.setItem('theme', 'dark');
+		};
+
+		lightButton.onclick = () => {
+			body.classList.replace('dark', 'light');
+			localStorage.setItem('theme', 'light');	
+		};	
+
+		solarButton.onclick = () => {
+			 if (body.classList.contains('solar')) {
+			 	body.classList.remove('solar'); 
+			 	solarButton.style.cssText = `
+			 		--bg-solar: var(--yellow);
+			 	`
+			 	solarButton.innerText = 'solarize';
+			 	localStorage.removeItem('isSolar');
+			 } else {
+			 	solarButton.style.cssText = `
+			 		--bg-solar: white;
+			 	`
+			 	body.classList.add('solar'); 
+			 	solarButton.innerText = 'normalize';
+
+			 	localStorage.setItem('isSolar', true);	
+			 }
+		};
+	</script>
 </body>
 </html>
