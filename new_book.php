1495bf1739899d74b7a257b7f51c71bb28d047d8
Nuno Pereira <nunocgpereira@gmail.com>
2020-04-10 15:31:41 +0100

    feat: add image cropping on uploading

--- new_book.php
+++ new_book.php
@@ -4,11 +4,22 @@
 	require ("includes/nav.inc.php");
 	require ("includes/dbh.inc.php");
 ?>
+
+<link rel="stylesheet" type="text/css" href="./includes/croppie.css">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="./includes/croppie.js"></script>
+
+
 	<div class="main-wrapper">
 		<h2>Adicionar Livro</h2>
 		<p>Mais um para a coleção!</p>
 		<div class="form-container">
-			<form class="new-book" method="POST" action="./includes/upload.inc.php" enctype="multipart/form-data">
+			<div class="new-book">
+			<!-- <form class="new-book" method="POST" action="./includes/upload.inc.php" enctype="multipart/form-data"> -->
 				<?php
 					if(isset($_GET["error"])) {
 						if($_GET["error"] == "emptyfields") {
@@ -28,7 +39,36 @@
 				<input type="hidden" name="userId" value="<?php echo $_SESSION['user_id'] ?>">
 
 				<label>Capa</label>
-				<input type="file" name="image" accept="image/*" capture="user">
+				<label for="upload_image" class="file-upload">Escolher ficheiro</label>
+				<input type="file" id="upload_image" name="upload_image" accept="image/*" capture="user">
+				<div id="uploaded_image"></div>
+
+				<div id="uploadimageModal" class="modal" role="dialog">
+				 <div class="modal-dialog">
+				  <div class="modal-content">
+				        <div class="modal-header">
+				          <button type="button" class="close" data-dismiss="modal">&times;</button>
+				          <h4 class="modal-title">Upload and Crop Image</h4>
+				        </div>
+				        <div class="modal-body">
+				          <div class="row">
+				       <div class="col-md-8 text-center">
+				        <div id="image_demo" style="width:350px; margin-top:30px"></div>
+				       </div>
+				       <div class="col-md-4" style="padding-top:30px;">
+				        <br />
+				        <br />
+				        <br/>
+				        <button class="btn btn-success crop_image">Crop and Upload Image</button>
+				     </div>
+				    </div>
+				        </div>
+				        <div class="modal-footer">
+				          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        </div>
+				     </div>
+				    </div>
+				</div>
 
 				<label>Título</label>
 				<input type="text" name="title" placeholder="Título" value="<?php if(isset($_GET['title'])) echo $_GET['title'] ?>">
@@ -71,7 +111,8 @@
 				</div>
 
 				<input type="submit" class="btn-submit" name="submit" value="Adicionar Livro">
-			</form>
+			<!-- </form> -->
+			</div>
 		</div>
 
 		<div id="gender-popup">
@@ -85,7 +126,7 @@
 		</div>
 	</div>
 
-	<script src="js/main.js"></script>
+	<!-- <script src="js/main.js"></script> -->
 	<script>
 		$(document).ready(function(){
 			$(document).on('click', '.submit-gender', function() {
@@ -114,6 +155,58 @@
 				}
 			});
 		});
+	</script>
+
+	<!-- <script src="./includes/croppie.js"></script> -->
+	<script>  
+		$(document).ready(function(){
+
+		 $image_crop = $('#image_demo').croppie({
+		    enableExif: true,
+		    viewport: {
+		      width:200,
+		      height:270,
+		      type:'square'
+		    },
+		    boundary:{
+		      width:300,
+		      height:300
+		    }
+		  });
+
+		  $('#upload_image').on('change', function(){
+		    var reader = new FileReader();
+		    reader.onload = function (event) {
+		      $image_crop.croppie('bind', {
+		        url: event.target.result
+		      }).then(function(){
+		        console.log('jQuery bind complete');
+		      });
+		    }
+		    reader.readAsDataURL(this.files[0]);
+		    $('#uploadimageModal').modal('show');
+		  });
+
+		  $('.crop_image').click(function(event){
+		  	console.log("Got here");
+		    $image_crop.croppie('result', {
+		      type: 'canvas',
+		      size: 'viewport'
+		    }).then(function(response){
+		      $.ajax({
+		        url:"./includes/upload.php",
+		        type: "POST",
+		        data:{"image": response},
+		        success:function(data)
+		        {
+		          $('#uploadimageModal').modal('hide');
+		          $('#uploaded_image').html(data);
+		        }
+		      });
+		    })
+		  });
+
+		});  
 	</script>
 </body>
 </html>
