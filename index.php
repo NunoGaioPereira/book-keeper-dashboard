31748c8aabe7c28c6a1bb51a5e946e478514d5af
Nuno Pereira <nunocgpereira@gmail.com>
2020-04-10 12:03:23 +0100

    feat: add readlist box to home page

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 	<div class="main-wrapper">
 		<h2>Home</h2>
 		<p>Dashboard Overview</p>
+
+		<div class="readlist">
+			<div class="header">
+				<h3>Read List</h3>
+				<a href="#/">+</a>
+			</div>
+			<div class="list-container">
+				
+			</div>
+		</div>
 	</div>
 
 	<!-- <script src="js/main.js"></script> -->
