1175c039091ae7585b8309b340c0e4951fbb09f1
Nuno Pereira <nunocgpereira@gmail.com>
2020-04-10 12:35:07 +0100

    add: container to home widgets

--- index.php
+++ index.php
@@ -7,134 +7,101 @@
 		<h2>Home</h2>
 		<p>Dashboard Overview</p>
 
-		<div class="readlist">
-			<div class="header">
-				<h3>Read List</h3>
-				<a href="#/">+</a>
+		<div class="home-container">
+			<div class="readlist">
+				<div class="header">
+					<h3>Read List</h3>
+					<a href="#/">+</a>
+				</div>
+				<div class="list-container">
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+					<div class="book">
+						<a href="">
+							<div class="text">
+								<h4>O Processo</h4>
+								<h5>Franz Kafka</h5>
+							</div>
+						</a>
+						<img src="./imgs/trash.png">
+					</div>
+				</div>
 			</div>
-			<div class="list-container">
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<a href="">
-						<div class="text">
-							<h4>O Processo</h4>
-							<h5>Franz Kafka</h5>
-						</div>
-					</a>
-					<img src="./imgs/trash.png">
-				</div>
-			</div>
-		</div>
 
-		<div class="readlist">
-			<div class="header">
-				<h3>Wish List</h3>
-				<a href="#/">+</a>
-			</div>
-			<div class="list-container">
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
-					</div>
-					<img src="./imgs/trash.png">
+			<div class="readlist">
+				<div class="header">
+					<h3>Wish List</h3>
+					<a href="#/">+</a>
 				</div>
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
+				<div class="list-container">
+					<div class="book">
+						<div class="text">
+							<h4>O Processo</h4>
+							<h5>Franz Kafka</h5>
+						</div>
+						<img src="./imgs/trash.png">
 					</div>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
-					</div>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
-					</div>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
-					</div>
-					<img src="./imgs/trash.png">
-				</div>
-				<div class="book">
-					<div class="text">
-						<h4>O Processo</h4>
-						<h5>Franz Kafka</h5>
-					</div>
-					<img src="./imgs/trash.png">
 				</div>
 			</div>
 		</div>
