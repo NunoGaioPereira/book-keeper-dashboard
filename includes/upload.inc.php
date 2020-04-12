112f845a252476b67175622dbdbebec3ffc57470
Nuno Pereira <nunocgpereira@gmail.com>
2020-04-12 16:41:46 +0100

    fix: remove temp img

--- includes/upload.inc.php
+++ includes/upload.inc.php
@@ -70,6 +70,8 @@ if(isset($_POST['submit'])) {
 							$stmt->bindParam(3, $userId);
 							$stmt->execute();
 
+							unlink('../imgs/books/temp.png');
+
 							/*$file_name = 'temp.png';
 							$path_user = '../imgs/books/'.$file_name.'';
 
@@ -99,58 +101,58 @@ if(isset($_POST['submit'])) {
 		}
 		else {
 			try {
-							$title = cleanInput($_POST['title']);
-							$author = cleanInput($_POST['author']);
-							$nationality = cleanInput($_POST['nationality']);
-							$date = cleanInput($_POST['book_date']);
-							$gender = cleanInput($_POST['gender']);
-							$userId = cleanInput($_POST['userId']);
-							$default = "default.png";
+				$title = cleanInput($_POST['title']);
+				$author = cleanInput($_POST['author']);
+				$nationality = cleanInput($_POST['nationality']);
+				$date = cleanInput($_POST['book_date']);
+				$gender = cleanInput($_POST['gender']);
+				$userId = cleanInput($_POST['userId']);
+				$default = "default.png";
 
-							$sql = "INSERT INTO books(userid, title, author, nationality, gender, date_, img) VALUES(?, ?, ?, ?, ?, ?, ?)";
-							$stmt = $conn->prepare($sql);
-							$stmt->bindParam(1, $userId);
-							$stmt->bindParam(2, $title);
-							$stmt->bindParam(3, $author);
-							$stmt->bindParam(4, $nationality);
-							$stmt->bindParam(5, $gender);
-							$stmt->bindParam(6, $date);
-							$stmt->bindParam(7, $default);
-							$stmt->execute();
+				$sql = "INSERT INTO books(userid, title, author, nationality, gender, date_, img) VALUES(?, ?, ?, ?, ?, ?, ?)";
+				$stmt = $conn->prepare($sql);
+				$stmt->bindParam(1, $userId);
+				$stmt->bindParam(2, $title);
+				$stmt->bindParam(3, $author);
+				$stmt->bindParam(4, $nationality);
+				$stmt->bindParam(5, $gender);
+				$stmt->bindParam(6, $date);
+				$stmt->bindParam(7, $default);
+				$stmt->execute();
 
-							// Get largest book id
-							$sql_latest_id = "SELECT id FROM books ORDER BY id DESC LIMIT 1";
-							$stmt = $conn->prepare($sql_latest_id);
-							$stmt->execute();
-							$book = $stmt->fetch();
-							if($book) { 
-								$book_id = $book['id'];
-							}
-							else {
-								$book_id = 1;
-							}
-							// $fileNameNew = $_SESSION['user_id']."_".$book_id.".png";
-							// // $fileNameNew = $_SESSION['user_id']."_".".".$fileActuelExt;
-							// $fileDestination = '../imgs/books/'.$fileNameNew;
-							// move_uploaded_file($fileTmpName, $fileDestination);
+				// Get largest book id
+				$sql_latest_id = "SELECT id FROM books ORDER BY id DESC LIMIT 1";
+				$stmt = $conn->prepare($sql_latest_id);
+				$stmt->execute();
+				$book = $stmt->fetch();
+				if($book) { 
+					$book_id = $book['id'];
+				}
+				else {
+					$book_id = 1;
+				}
+				// $fileNameNew = $_SESSION['user_id']."_".$book_id.".png";
+				// // $fileNameNew = $_SESSION['user_id']."_".".".$fileActuelExt;
+				// $fileDestination = '../imgs/books/'.$fileNameNew;
+				// move_uploaded_file($fileTmpName, $fileDestination);
 
-							/*$file_name = 'temp.png';
-							$path_user = '../imgs/books/'.$file_name.'';
+				/*$file_name = 'temp.png';
+				$path_user = '../imgs/books/'.$file_name.'';
 
-							if (!file_exists($path_user.$file_name)) 
-							{                   
-								
-							} else {
-							  unlink($path_user.$file_name);
-							}*/
+				if (!file_exists($path_user.$file_name)) 
+				{                   
+					
+				} else {
+				  unlink($path_user.$file_name);
+				}*/
 
-							header("Location: ../booknotes.php?book=".$book_id."");
-						}
-						catch(PDOException $e){
-							header("Location: ../new_book.php?create=error");
-							throw $e;
-							exit();
-						}
+				header("Location: ../booknotes.php?book=".$book_id."");
+			}
+			catch(PDOException $e){
+				header("Location: ../new_book.php?create=error");
+				throw $e;
+				exit();
+			}
 		}
 	}
 }
